2510695dca277b4ec515e21b79dc003394b886c1
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-06-06 16:03:09 +0300

    Add Task 02 koord

---
+++ Task02_koord/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите прогу, которая по заданному номеру четверти, показывает диапозон возможных 
+//координат точек в этой  четверти (х и у).
+Console.Write("Введите четверть координат: ");
+int numCoord = int.Parse(Console.ReadLine());
+
+void GetRange(int number)
+{
+    if(number == 1) Console.WriteLine("х = от 0 до ++. у = от 0 до ++ ");
+    else if(number == 2) Console.WriteLine("х = от 0 до --. у = от 0 до ++ ");
+    else if(number == 3) Console.WriteLine("х = от 0 до --. у = от 0 до -- ");
+    else if(number == 4) Console.WriteLine("х = от 0 до ++. у = от 0 до -- ");
+    
+}
+GetRange(numCoord);
