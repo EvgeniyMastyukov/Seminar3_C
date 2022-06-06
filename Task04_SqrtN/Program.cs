76c0413a6b73527b3901303a2120fe8f7e437f88
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-06-06 16:04:36 +0300

    Add Task04 Sqrtn

---
+++ Task04_SqrtN/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите прогу, которая на вход принимает число N и выдаете таблицу квадратов чисел 
+//от 1 до N  5-> 1,4,9,16,25   2-> 1,4.
+// Console.Write("Введите любое число N: ");
+// int num = int.Parse(Console.ReadLine());
+// for  (int i = 1; i<=num; i++)
+// {
+//     double sqrt = Math.Pow(i,2); //i*i;
+//     Console.Write($" -> {sqrt}");
+// }
+
+Console.Write("Введите любое число N: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("|{0,5}|{1,5}|", "Число", "Квадрат");
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine("|{0,5}|{1,5}|", i, i * i);
+}
